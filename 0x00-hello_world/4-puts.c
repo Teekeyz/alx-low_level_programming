80dc1fde354c71b457a97292ed564579396bbb02
Teekeyz <godsownzannu@gmail.com>
2022-10-14 22:27:42 -0700

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+/**
+ * main - Printing
+ *
+ * Return: Always 0 (Successful)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
