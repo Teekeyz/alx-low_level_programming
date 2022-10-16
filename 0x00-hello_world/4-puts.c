339ac0aafb717b41aeb53e75bae7da7df9b80dbc
Teekeyz <godsownzannu@gmail.com>
2022-10-16 11:33:07 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
-#!/bin/bash
 #include <stdio.h>
-/**
- * main - Printing
- *
- * Return: Always 0 (Successful)
- */
+/*
+ * main - Prints - "Programming is like building a multilingual puzzle
+ * followed by newline
+ * Return 0 always.
+*/
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-
+	puts("Programming is like building a multilingual puzzle\n");
 	return (0);
 }
